c4567eec57e810b378ef4c02305015558078f725
VathanSilva <vathansilva@gmail.com>
2024-08-27 00:22:16 +0530

    issue fixed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from 'next/head';
 import Image from "next/image";
 import React from "react";
@@ -99,7 +98,6 @@ export default function Home() {
         </section>
         <Footer />
         <Analytics />
-        <SpeedInsights />
       </main>
     </div>
   );
